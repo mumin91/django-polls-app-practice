80f343cce24327e21c7d5c0b06e9739bb11aff80
Mumin <r.muminur@gmail.com>
2018-08-31 21:29:40 +0600

    404 exception handling shortcut

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 
 from .models import Question
@@ -14,10 +14,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question not found!")
+
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
